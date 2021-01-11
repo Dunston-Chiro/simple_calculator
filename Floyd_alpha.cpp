d4dcf506393fd46eab80a191b539db250e74edde
Dunston-Chiro <77020829+Dunston-Chiro@users.noreply.github.com>
2021-01-11 22:05:08 +0530

    Add files via upload

---
+++ Floyd_alpha.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N, i, j;
+    char alpha='A';
+
+    cin>>N;
+
+    cout<<"\n\n "<<'*'<<"\n";
+
+    for(i=1;i<N;i++)
+    {
+        cout<<'*';
+
+        for(j=1;j<=i;j++)
+        {
+            cout<<alpha<<'*';
+        }
+
+        cout<<"\n";
+        alpha++;
+    }
+}
